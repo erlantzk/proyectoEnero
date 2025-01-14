da456bea023f81da163058c70283b18f77c44780
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-14 10:33:37 +0100

    Constructor -Iker

--- Libros/src/Libros/Libros.java
+++ Libros/src/Libros/Libros.java
@@ -6,4 +6,12 @@ public class Libros {
 	int id;
 	int añoPublicacion;
 	int copiasDisponibles;
+	
+	public Libros (String t, String a, int i, int ap, int cd) {
+		this.titulo = t;
+		this.autor = a;
+		this.id = i;
+		this.añoPublicacion = ap;
+		this.copiasDisponibles = cd;
+	}
 }

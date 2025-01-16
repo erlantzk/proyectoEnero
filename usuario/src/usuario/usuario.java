2b3ad97e80f7d79a287eefae8158d3d10e07330c
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-16 12:04:54 +0100

    clase actualizada eneritz

--- usuario/src/usuario/usuario.java
+++ usuario/src/usuario/usuario.java
@@ -1,14 +1,21 @@
 package usuario;
 
 public class usuario {
-	
-	String nombre;
-	int ID;
-	String[] rol = new String[3];
+    
+    String nombre;
+    int ID;
+    String[] rol = new String[3];
 
-	rol[0]="estudiante";
-	rol[1]="profesor";
-	rol[2]="invitado";
-	
+    public usuario() {
+        rol[0] = "estudiante";
+        rol[1] = "profesor";
+        rol[2] = "invitado";
+    }
+
+    public usuario(String nombre, int ID) {
+        this();
+        this.nombre = nombre;
+        this.ID = ID;
+    }
 }
-}
+

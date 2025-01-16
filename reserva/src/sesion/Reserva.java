addfac372321827950fa1ccc277b9b33ec8d9d1a
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-16 12:50:03 +0100

    nuevos cambios y añadido los get y set de idReserva -Illart

--- reserva/src/sesion/Reserva.java
+++ reserva/src/sesion/Reserva.java
@@ -15,14 +15,20 @@ public class Reserva {
     }
     //Metodo para crear una reserva//
     public void crearReserva() {
-    	System.out.println("Reserva creada "+idReserva+" por el usuario "+usuario+" para el libro "+libro+" en la fecha "+fechaReserva);
+    	System.out.println("Reserva creada "+idReserva+" por el usuario "+usuario+" para el libro "+libro+" en la fecha "+fechaReserva+".");
     }
     //metodo lista de reservas//
 	public void ListarReservas() {
-	System.out.print("idReserva:"+idReserva+" Usuario:"+usuario+" Libro:"+libro+" Fecha de reserva:"+fechaReserva); 
+	System.out.print("ID de Reserva:"+idReserva+" Usuario:"+usuario+" Libro:"+libro+" Fecha de reserva:"+fechaReserva+"."); 
 	}
 	//metodo liberar reserva//
 	public void liberarReserva() {
-		System.out.print("La reserva "+idReserva+" del usuario "+usuario+" ha sido liberada");
+		System.out.print("La reserva "+idReserva+" del usuario "+usuario+" ha sido liberada.");
+	}
+	public int getIdReserva() {
+		return idReserva;
+	}
+	public void setIdReserva(int idReserva) {
+		this.idReserva = idReserva;
 	}
 }

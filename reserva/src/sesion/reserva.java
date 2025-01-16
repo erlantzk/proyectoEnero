48520b0ffe1b931759a2f6d38ebd8ccf269ff66e
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-16 11:57:27 +0100

    Actualizacion del constructor -illart

--- reserva/src/sesion/reserva.java
+++ reserva/src/sesion/reserva.java
@@ -1,5 +1,17 @@
 package sesion;
 
-public class reserva {
-	int IDreserva;
-}
+public class Reserva {
+    private static int contadorReservas = 0;
+    private int idReserva;
+    private String usuario;
+    private String libro;
+    private String fechaReserva;
+  
+    //constructor//
+    public Reserva(Usuario usuario, Libro libro) {
+        this.idReserva = ++contadorReservas;
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaReserva = fechaReserva.now();
+    }
+

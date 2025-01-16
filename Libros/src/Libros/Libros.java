f7259e07fb8771281e94647bebdfae73928d43e6
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-16 12:36:16 +0100

    Modificacion del array ListaDeReservas y creacion del metodo añadirReserva -Iker

--- Libros/src/Libros/Libros.java
+++ Libros/src/Libros/Libros.java
@@ -1,32 +1,44 @@
 package Libros;
 
+import java.util.ArrayList;
 
 public class Libros {
-	String titulo;
-	String autor;
-	int id;
-	int añoPublicacion;
-	int copiasDisponibles;
-	reserva [] listaDeReserva;
-	
-	public Libros (String t, String a, int i, int ap, int cd) {
-		this.titulo = t;
-		this.autor = a;
-		this.id = i;
-		this.añoPublicacion = ap;
-		this.copiasDisponibles = cd;
-	}
-	
-	public void consultarLibro(Libros libro, Libros id) {
-		System.out.println("Titulo: " + titulo + " Autor: " + autor + " ID: " + " Año de publicación " + añoPublicacion + "Copias disponibles: " + copiasDisponibles);
-	}
-	
-	public void listarLibros(Libros [] libros){
-		for (Libros contador : libros) {
-			System.out.println(contador.titulo);
-		}
-	}
-	
-	
-	
+    String titulo;
+    String autor;
+    int id;
+    int añoPublicacion;
+    int copiasDisponibles;
+    ArrayList<reserva> listaDeReservas;
+
+    public Libros(String t, String a, int i, int ap, int cd) {
+        this.titulo = t;
+        this.autor = a;
+        this.id = i;
+        this.añoPublicacion = ap;
+        this.copiasDisponibles = cd;
+        this.listaDeReservas = new ArrayList<>();
+    }
+
+    public void consultarLibro() {
+        System.out.println("Titulo: " + titulo + " | Autor: " + autor + " | ID: " + id +
+                " | Año de publicación: " + añoPublicacion + " | Copias disponibles: " + copiasDisponibles);
+    }
+
+    public void listarLibros(Libros[] libros) {
+        for (Libros libro : libros) {
+            System.out.println(libro.titulo);
+        }
+    }
+
+    public boolean añadirReserva(reserva nuevaReserva) {
+        if (copiasDisponibles > 0) {
+            copiasDisponibles--; 
+            listaDeReservas.add(nuevaReserva);
+            System.out.println("Reserva añadida con éxito para el libro: " + titulo);
+            return true;
+        } else {
+            System.out.println("No hay copias disponibles para reservar el libro: " + titulo);
+            return false;
+        }
+    }
 }

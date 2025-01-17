be1c28203d10a639a53c4eda3aa2a2d689f13cb7
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-17 13:29:49 +0100

    Los cambios para que todas las clases esten bien conectadas -illart

--- reserva/src/sesion/reserva.java
+++ reserva/src/sesion/reserva.java
@@ -1,37 +1,47 @@
 package sesion;
 
-	//Atributos//
-public class Reserva {
+public class reserva {
+
     private int idReserva;
-    private String usuario;
-    private String libro;
+    private usuario usuario;
+    private Libros libro;
     private String fechaReserva;
-  
+
     //constructor//
-    public Reserva(int idReserva,String usuario,String libro,String fechaReserva) {
+    public reserva(int idReserva,usuario usuario,Libros libro,String fechaReserva) {
+
         this.idReserva = idReserva;
         this.usuario = usuario;
         this.libro = libro;
         this.fechaReserva = fechaReserva;
     }
+
     //Metodo para crear una reserva//
     public void crearReserva() {
     	System.out.println("Reserva creada "+idReserva+" por el usuario "+usuario+" para el libro "+libro+" en la fecha "+fechaReserva+".");
+
     }
+
     //metodo lista de reservas//
 	public void ListarReservas() {
 	System.out.print("ID de Reserva:"+idReserva+" Usuario:"+usuario+" Libro:"+libro+" Fecha de reserva:"+fechaReserva+"."); 
+
 	}
+
 	//metodo liberar reserva//
 	public void liberarReserva() {
 		System.out.print("La reserva "+idReserva+" del usuario "+usuario+" ha sido liberada.");
+
 	}
-	 //get IdReserva//
+	
 	public int getIdReserva() {
 		return idReserva;
+
 	}
-	//set IdReserva//
+
 	public void setIdReserva(int idReserva) {
 		this.idReserva = idReserva;
+
 	}
+
 }

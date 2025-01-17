ef97d1eb7206522f821a260cc78da18a5b204435
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-17 12:05:34 +0100

    implementacion de prestamo en usuario (eneritz)

--- usuario/src/usuario/usuario.java
+++ usuario/src/usuario/usuario.java
@@ -5,6 +5,7 @@ public class usuario {
     String nombre;
     int ID;
     String[] rol = new String[3];
+     List<Libro> librosPrestados;
 
     public usuario() {
         rol[0] = "estudiante";
@@ -25,6 +26,45 @@ public class usuario {
     		System.out.println(contador.nombre);
     	}
     }
+    public boolean puedeRealizarPrestamo() {
+        int limitePrestamos = obtenerLimitePrestamosPorRol();
+        return librosPrestados.size() < limitePrestamos;
+    }
+    public int obtenerLimitePrestamosPorRol() {
+        switch (rol.toLowerCase()) {
+            case "estudiante":
+                return 3;
+            case "profesor":
+                return 5;
+            case "invitado":
+                return 1;
+            default:
+                return 0; 
+        }
+    }
+    public void añadirLibroPrestado(Libro libro) {
+        if (puedeRealizarPrestamo()) {
+            librosPrestados.add(libro);
+            System.out.println("Libro añadido al usuario: " + nombre);
+        } else {
+            System.out.println("No se pueden añadir más libros. Límite alcanzado.");
+        }
+    }
+
+    public void removerLibroPrestado(Libro libro) {
+        if (librosPrestados.contains(libro)) {
+            librosPrestados.remove(libro);
+            System.out.println("Libro devuelto por el usuario: " + nombre);
+        } else {
+            System.out.println("El libro no estaba en la lista de préstamos.");
+        }
+    }
+
+    public void registrarUsuario(Usuario usuario) {
+        System.out.println("Usuario registrado: Nombre - " + usuario.getNombre() + ", ID - " + usuario.getId());
+    }
+    
+    
      
 }
 
